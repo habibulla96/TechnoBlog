fcd7b5251e1d5082a55ab0a4f5ecaf1cd931d8dc
Habib <habib.ushurov@gmail.com>
2021-01-09 16:07:31 +0600

    test commit

--- technoblog/settings.py
+++ technoblog/settings.py
@@ -208,3 +208,10 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'django-cache'),
+    }
+}
--- technoblog/urls.py
+++ technoblog/urls.py
@@ -22,9 +22,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
